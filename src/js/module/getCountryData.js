a17d9dab92ea2f6386167e81216895d64ac438d4
kredo405 <aleksandr.kreidich@yandex.ru>
2022-01-05 23:49:14 +0300

    add changeVaalueTogle

--- src/js/module/getCountryData.js
+++ src/js/module/getCountryData.js
@@ -1,14 +1,28 @@
 import axios from 'axios';
 import { arrCountries } from './../../data/countries';
 function getCountryData() {
-  const dropdownCountry = document.querySelector(
-    '.search-panel__dropdown-menu--country'
-  );
+  const dropdownCountrylist = document.querySelector(
+      '.search-panel__dropdown-menu--country'
+    ),
+    buttonCountry = document.querySelector('.search-panel__button-toggle');
+
   arrCountries.forEach((el) => {
     let li = document.createElement('li');
-    li.innerHTML = `<a class="dropdown-item search-panel__item" href="#">${el.name}<img src='${el.flag}' class='search-panel__flag'></a>`;
-    dropdownCountry.append(li);
+    li.innerHTML = `<a class="dropdown-item search-panel__item" href="#">
+    ${el.name}<img src='${el.flag}' class='search-panel__flag'></a>`;
+    dropdownCountrylist.append(li);
   });
+
+  function changeVaalueTogle(country) {
+    let countryItem = document.querySelectorAll('.search-panel__item');
+    countryItem.forEach((el) => {
+      el.addEventListener('click', (e) => {
+        country.innerHTML = e.target.innerHTML;
+      });
+    });
+  }
+
+  changeVaalueTogle(buttonCountry);
 }
 
 export default getCountryData;
