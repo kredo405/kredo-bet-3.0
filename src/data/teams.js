98fb2b5e18bd028a78f34964e47240e75684e504
kredo405 <aleksandr.kreidich@yandex.ru>
2022-01-09 03:16:56 +0300

    added price and playstyle for teams from albania

---
+++ src/data/teams.js
@@ -0,0 +1,284 @@
+const teams = {
+  England: {
+    'Manchester City': {
+      rating: 11.6,
+      name: 'Manchester City',
+      id: '50',
+      logo: 'https://media.api-sports.io/football/teams/50.png',
+    },
+    Chelsea: {
+      rating: 8.2,
+      name: 'Chelsea',
+      id: '49',
+      logo: 'https://media.api-sports.io/football/teams/49.png',
+    },
+
+    Liverpool: {
+      rating: '8.75',
+      name: 'Liverpool',
+      id: '40',
+      logo: 'https://media.api-sports.io/football/teams/40.png',
+    },
+
+    Arsenal: {
+      rating: '7.65',
+      name: 'Arsenal',
+      id: '42',
+      logo: 'https://media.api-sports.io/football/teams/42.png',
+    },
+
+    'West Ham': {
+      rating: '7.2',
+      name: 'West Ham',
+      id: '48',
+      logo: 'https://media.api-sports.io/football/teams/48.png',
+    },
+
+    Tottenham: {
+      rating: '7.555555555555555',
+      name: 'Tottenham',
+      id: '47',
+      logo: 'https://media.api-sports.io/football/teams/47.png',
+    },
+
+    'Manchester United': {
+      rating: '6.526315789473685',
+      name: 'Manchester United',
+      id: '33',
+      logo: 'https://media.api-sports.io/football/teams/33.png',
+    },
+
+    Wolves: {
+      rating: '5.68421052631579',
+      name: 'Wolves',
+      id: '39',
+      logo: 'https://media.api-sports.io/football/teams/39.png',
+    },
+
+    Brighton: {
+      rating: '4.578947368421053',
+      name: 'Brighton',
+      id: '51',
+      logo: 'https://media.api-sports.io/football/teams/51.png',
+    },
+
+    Leicester: {
+      rating: '5.166666666666666',
+      name: 'Leicester',
+      id: '46',
+      logo: 'https://media.api-sports.io/football/teams/46.png',
+    },
+
+    'Crystal Palace': {
+      rating: '3.5999999999999996',
+      name: 'Crystal Palace',
+      id: '52',
+      logo: 'https://media.api-sports.io/football/teams/52.png',
+    },
+
+    Brentford: {
+      rating: '4.2105263157894735',
+      name: 'Brentford',
+      id: '55',
+      logo: 'https://media.api-sports.io/football/teams/55.png',
+    },
+
+    'Aston Villa': {
+      rating: '4.578947368421053',
+      name: 'Aston Villa',
+      id: '66',
+      logo: 'https://media.api-sports.io/football/teams/66.png',
+    },
+
+    Southampton: {
+      rating: '2.7368421052631584',
+      name: 'Southampton',
+      id: '41',
+      logo: 'https://media.api-sports.io/football/teams/41.png',
+    },
+
+    Everton: {
+      rating: '3.333333333333333',
+      name: 'Everton',
+      id: '45',
+      logo: 'https://media.api-sports.io/football/teams/45.png',
+    },
+
+    Leeds: {
+      rating: '2.2631578947368425',
+      name: 'Leeds',
+      id: '63',
+      logo: 'https://media.api-sports.io/football/teams/63.png',
+    },
+
+    Watford: {
+      rating: '2.166666666666667',
+      name: 'Watford',
+      id: '38',
+      logo: 'https://media.api-sports.io/football/teams/38.png',
+    },
+
+    Burnley: {
+      rating: '0.5882352941176472',
+      name: 'Burnley',
+      id: '44',
+      logo: 'https://media.api-sports.io/football/teams/44.png',
+    },
+
+    Newcastle: {
+      rating: '-0.10526315789473695',
+      name: 'Newcastle',
+      id: '34',
+      logo: 'https://media.api-sports.io/football/teams/34.png',
+    },
+
+    Norwich: {
+      rating: '-0.2105263157894739',
+      name: 'Norwich',
+      id: '71',
+      logo: 'https://media.api-sports.io/football/teams/71.png',
+    },
+  },
+  Albania: {
+    Tirana: {
+      name: 'Tirana',
+      id: '694',
+      logo: 'https://media.api-sports.io/football/teams/694.png',
+      price: 4.85,
+      style: 'att',
+    },
+
+    'FK Kukesi': {
+      name: 'FK Kukesi',
+      id: '577',
+      logo: 'https://media.api-sports.io/football/teams/577.png',
+      price: 4.23,
+      style: 'att',
+    },
+
+    Laci: {
+      name: 'Laci',
+      id: '2256',
+      logo: 'https://media.api-sports.io/football/teams/2256.png',
+      price: 4.28,
+      style: 'att',
+    },
+
+    'Vllaznia Shkodër': {
+      name: 'Vllaznia Shkodër',
+      id: '3339',
+      logo: 'https://media.api-sports.io/football/teams/3339.png',
+      price: 4.88,
+      style: 'balance',
+    },
+
+    Partizani: {
+      name: 'Partizani',
+      id: '708',
+      logo: 'https://media.api-sports.io/football/teams/708.png',
+      price: 5.48,
+      style: 'att',
+    },
+
+    'Teuta Durrës': {
+      name: 'Teuta Durrës',
+      id: '3320',
+      logo: 'https://media.api-sports.io/football/teams/3320.png',
+      price: 3.3,
+      style: 'def',
+    },
+
+    'Skenderbeu Korce': {
+      name: 'Skenderbeu Korce',
+      id: '605',
+      logo: 'https://media.api-sports.io/football/teams/605.png',
+      price: 3.28,
+      style: 'balance',
+    },
+
+    'Dinamo Tirana': {
+      name: 'Dinamo Tirana',
+      id: '3326',
+      logo: 'https://media.api-sports.io/football/teams/3326.png',
+      price: 4.48,
+      style: 'balance',
+    },
+
+    'Egnatia Rrogozhinë': {
+      name: 'Egnatia Rrogozhinë',
+      id: '3327',
+      logo: 'https://media.api-sports.io/football/teams/3327.png',
+      price: 2.75,
+      style: 'att',
+    },
+
+    'Kastrioti Krujë': {
+      name: 'Kastrioti Krujë',
+      id: '3319',
+      logo: 'https://media.api-sports.io/football/teams/3319.png',
+      price: 2.45,
+      style: 'balance',
+    },
+  },
+  Armenia: {
+    Alashkert: {
+      name: 'Alashkert',
+      id: '582',
+      logo: 'https://media.api-sports.io/football/teams/582.png',
+    },
+
+    'Noah Jūrmala': {
+      name: 'Noah Jūrmala',
+      id: '16462',
+      logo: 'https://media.api-sports.io/football/teams/16462.png',
+    },
+
+    'Banants Yerevan': {
+      name: 'Banants Yerevan',
+      id: '2276',
+      logo: 'https://media.api-sports.io/football/teams/2276.png',
+    },
+
+    Ararat: {
+      name: 'Ararat',
+      id: '3682',
+      logo: 'https://media.api-sports.io/football/teams/3682.png',
+    },
+
+    'Ararat-Armenia': {
+      name: 'Ararat-Armenia',
+      id: '3683',
+      logo: 'https://media.api-sports.io/football/teams/3683.png',
+    },
+
+    Van: {
+      name: 'Van',
+      id: '6286',
+      logo: 'https://media.api-sports.io/football/teams/6286.png',
+    },
+
+    'Pyunik Yerevan': {
+      name: 'Pyunik Yerevan',
+      id: '709',
+      logo: 'https://media.api-sports.io/football/teams/709.png',
+    },
+
+    Lori: {
+      name: 'Lori',
+      id: '3685',
+      logo: 'https://media.api-sports.io/football/teams/3685.png',
+    },
+
+    Shirak: {
+      name: 'Shirak',
+      id: '3686',
+      logo: 'https://media.api-sports.io/football/teams/3686.png',
+    },
+
+    Gandzasar: {
+      name: 'Gandzasar',
+      id: '688',
+      logo: 'https://media.api-sports.io/football/teams/688.png',
+    },
+  },
+};
